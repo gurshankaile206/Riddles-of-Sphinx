3f8a6d059eb5821aa0ec4824c5f791b255eecd02
Gurshan Kaile <gurshankaile206@gmail.com>
2021-07-12 16:33:08 -0700

    finsh functionality
    co-authored-by: Allister <allisterkmoon@gmail.com>

--- Sphinx.cs
+++ Sphinx.cs
@@ -4,11 +4,15 @@ using System.Collections.Generic;
 class Program
 {
   static List<string> riddles = new List<string>(new string[] {
-    "What goes on four feet in the morning, two feet at noon, and three feet in the evening?"
+    "What goes on four feet in the morning, two feet at noon, and three feet in the evening?",
+    "I come from a mine and get surrounded by wood always. Everyone uses me. What am I?",
+    "A box without hinges, key, or lid. Yet inside golden treasure is hid."
   });
 
   static List<string> answers = new List<string>(new string[] {
-    "person"
+    "person",
+    "pencil lead",
+    "an egg"
   });
 
 
@@ -16,26 +20,45 @@ class Program
   {
     Console.WriteLine("Welcome to the riddle of the Sphinx! It is an inquisitive sort, answer its questions well or be devoured!");
     Console.WriteLine("--------------");
-    Console.WriteLine(riddles[0]);
 
-    while (true) {
-      // do this thing
-      string answer = Console.ReadLine();
-      bool result = Sphinx(answer);
+    ShuffleRiddles();
+
+    for (int index = 0; index < riddles.Count;) 
+    {
+      Console.WriteLine(riddles[index]);
+      string answer = Console.ReadLine().ToLower();
+      bool result = Sphinx(answer, index);
+     
       if (result)
-      {
-        Console.WriteLine("Correct! You win");
-        break;
+      { 
+        index++;
+        Console.WriteLine("Correct! Moving onto the next riddle");
       } else {
         Console.WriteLine("That's incorrect! I am supposed to eat you for getting the wrong answer but I will be nice for now, TRY AGAIN!");
       }
     }
-
-    
   }
 
-  static public bool Sphinx(string input) 
+  static public void ShuffleRiddles () {
+    Random rand = new Random();
+    
+    for (int currentIndex = 0; currentIndex < riddles.Count; currentIndex++) {
+      int nextIndex = rand.Next(0, riddles.Count);
+
+      string currentRiddle = riddles[currentIndex];
+      string currentAnswer = answers[currentIndex];
+      string nextRiddle = riddles[nextIndex];
+      string nextAnswer = answers[nextIndex];
+
+      riddles[currentIndex] = nextRiddle;
+      answers[currentIndex] = nextAnswer;
+      riddles[nextIndex] = currentRiddle;
+      answers[nextIndex] = currentAnswer;
+    }
+  }
+
+  static public bool Sphinx(string input, int index) 
   { 
-    return (input == answers[0]);
+    return (input == answers[index]);
   }
 }
